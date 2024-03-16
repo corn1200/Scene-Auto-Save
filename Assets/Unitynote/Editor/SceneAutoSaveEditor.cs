972e398a235b444129a698a958f182ae6a2e91e6
김현승 <corn120033@gmail.com>
2024-03-16 19:00:21 +0900

    메뉴 경로에 커스텀 에디터 항목 표출 기능 추가

---
+++ Assets/Unitynote/Editor/SceneAutoSaveEditor.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+public class SceneAutoSaveEditor : EditorWindow
+{
+    private static SceneAutoSaveEditor window;
+
+    private GUIStyle titleStyle;
+
+    [MenuItem("Window/SceneAutoSave/SceneAutoSave - Custom Editor")]
+    private static void Setup()
+    {
+        window = GetWindow<SceneAutoSaveEditor>();
+        window.titleContent = new GUIContent("SceneAutoSave");
+        window.minSize = new Vector2(300, 300);
+        window.maxSize = new Vector2(1920, 1080);
+    }
+
+    private void Awake()
+    {
+        titleStyle = new GUIStyle(EditorStyles.label);
+        titleStyle.fontStyle = FontStyle.Bold;
+        titleStyle.normal.textColor = Color.yellow;
+    }
+
+    private void OnGUI()
+    {
+        EditorGUILayout.LabelField("Scene Auto Save", titleStyle);
+    }
+}
